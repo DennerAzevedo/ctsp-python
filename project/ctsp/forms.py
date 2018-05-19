14bad9d2a00363dc42631f78744de5a0a360838d
José Tobias <a16007@bcc.unifal-mg.edu.br>
2018-05-19 20:25:32 -0300

    working on querying database

--- project/ctsp/forms.py
+++ project/ctsp/forms.py
@@ -7,12 +7,12 @@ class ProjectForm(forms.ModelForm):
         attrs={
             'class': 'form-control',
         }
-    ), label='project_name do Project', max_length=Project.project_name_max_length)
+    ), label='Project Name', max_length=Project.project_name_max_length)
     project_init = forms.CharField(widget=forms.TextInput(
         attrs={
             'class': 'form-control',
         }
-    ), label='Sigla', max_length=Project.project_init_max_length)
+    ), label='Scrum Project ID', max_length=Project.project_init_max_length)
     proiect_start_date = forms.DateField(widget=forms.DateInput(
         attrs={
             'class':'control-label m-3', 'id':'from_forms', 'placeholder':"MM/DD/YYY",
@@ -27,3 +27,14 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         fields = ('project_name', 'project_init', 'proiect_start_date', 'project_final_date',)
+
+class QueryProjectForm(forms.ModelForm):
+    project_name_or_ID = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ), label='Project Name or ID', max_length=Project.project_name_max_length)
+
+    class Meta:
+        model = Project
+        fields = ('project_name_or_ID',)
--- project/ctsp/views.py
+++ project/ctsp/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponseRedirect, reverse
-from .forms import ProjectForm
+from django.shortcuts import render, HttpResponseRedirect, reverse, redirect
+from .forms import ProjectForm, QueryProjectForm
 from .models import Project
 
 # Create your views here.
@@ -7,24 +7,37 @@ from .models import Project
 
 def index(request):
     form = ProjectForm()
+    query = QueryProjectForm()
+    context_dict = {'form': form, 'query': query}
+
+    return render(request, 'ctsp/index.html', context=context_dict)
+
+
+def create_project_modal(request):
+    form = ProjectForm()
     context_dict = {'form': form}
 
     if request.method == 'POST':
         form = ProjectForm(request.POST)
+
         if form.is_valid():
             project = form.save(commit=True)
             context_dict['project'] = Project.objects.filter(pk=project.pk)
-            print(project.pk)
-            return render(request, 'ctsp/project_welcome.html', context_dict)
-            # return HttpResponseRedirect(reverse('ctsp:project_welcome', args=(project.pk,)))
-    else:
-        print(form.errors)
+            return render(request, 'ctsp/welcome.html', context=context_dict)
     return render(request, 'ctsp/index.html', context_dict)
 
+def query_project_modal(request):
+    query = QueryProjectForm()
+    context_dict = {'query': query}
+    if request.method == 'POST':
+        query = QueryProjectForm(request.POST)
+        print(query)
+
+    return render(request, 'ctsp/index.html', context_dict)
 
 def about(request):
     return render(request, 'ctsp/about.html')
 
 
 def project_welcome(request):
-    return render(request, 'ctsp/project_welcome.html')
+    return render(request, 'ctsp/welcome.html')
