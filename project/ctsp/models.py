0afb6423c7614babfc2bfdd43b5cb5e3aa45f2c4
DennerAzevedo <DennerAzevedo@github.com>
2018-06-17 11:19:37 -0300

    Add das outras tabelas

--- project/ctsp/models.py
+++ project/ctsp/models.py
@@ -12,10 +12,35 @@ class Project(models.Model):
     To learn more about Django database models refer to: https://docs.djangoproject.com/en/2.0/topics/db/models/
     '''
 
-    project_name = models.CharField(max_length=128, null=False)
+    project_name = models.CharField(max_length=128, null=False, primary_key= True)
     project_name_max_length = project_name.max_length
     project_start_date = models.DateField(default=timezone.now, null=True)
     project_final_date = models.DateField(default=timezone.now, null=True)
-
+	
     def _str_(self):
         return self.project_name_max_length
+		
+		
+	   
+	class Usuario(models.Model):
+		usuario_name = models.CharField(max_length=30, null=False)
+		usuario_name_max_length = usuario_name.max_length
+		usuario_sobrename = models.CharField(max_lenght=100, null=False)
+		usuario_sobrename_max_length = usuario_sobrename.max_length
+		usuario_nascimento = models.DataField(default=timezone.now, null = True)
+		usuario_telefone = models.CharField('Telefone para Contato', max_length=13,blank=True, null=True)
+		usuario_habilidades = models.CharField(max_lenght = 300, null = True)
+		usuario_email = models.CharField(max_lenght = 50, null =False)
+		usuario_senha = models.CharField(max_lenght = 100, null = False, primary_key = True)
+		usuario_membroId = models.ForeignKey('membroDoTime_id', on_delete= models.CASCADE)
+		
+    def _str_(self):
+        return self.usuario_email_max_length
+		
+		
+	class MembroDoTime(models.Model):
+		membroDoTime_id = models.IntegerField(primary_key=True)
+		membroDoTime_project = models.ForeignKey('project_name', on_delete= models.CASCADE)
+		membroDoTime_email =  models.ForeignKey('usuario_email', on_delete= models.CASCADE)
+		membroDoTime_cargos = models.CharField(max_lenght = 50, null =False);
+		
